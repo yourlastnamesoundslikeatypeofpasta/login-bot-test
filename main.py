21849b12cfeae4db462b28d94df1c14f6379331a
Christian Zagazeta <christianzagazeta@icloud.com>
2021-08-20 19:45:47 -0400

    Added Calculator Form Modal Added Submit Results Modal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from slack_bolt import App
 from slack_sdk.errors import SlackApiError
@@ -19,14 +20,151 @@ def get_error_msg_str(command_name):
     return f'`Error: Stats not entered correctly. Enter: "/{command_name} help" for help`'
 
 
-# Listen to the app_home_opened Events API event to hear when a user opens your app from the sidebar
+# Listen to the app_home_opened Events API event to hear when a user opens your app from the sidebarAd
 @app.event("app_home_opened")
-def app_home_opened(event, client, logger):
-    user = event["user"]
+def app_home_opened(event, logger):
+    @app.action('score_home_button')
+    def score_home_button_click(ack, body):
+        ack()
+        trigger_id = body['trigger_id']
+        try:
+            # open score input field view
+            score_view_result = app.client.views_open(
+                trigger_id=trigger_id,
+                view={
+                    "type": "modal",
+                    "callback_id": "calc_score_modal",
+                    "title": {
+                        "type": "plain_text",
+                        "text": "Enter Production Stats"
+                    },
+                    "submit": {
+                        "type": "plain_text",
+                        "text": "Calculate"
+                    },
+                    "blocks": [
+                        {
+                            "type": "input",
+                            "block_id": "block_package",
+                            "element": {
+                                "type": "plain_text_input",
+                                "action_id": "sl_input",
+                                "placeholder": {
+                                    "type": "plain_text",
+                                    "text": "300"
+                                }
+                            },
+                            "label": {
+                                "type": "plain_text",
+                                "text": "Packages"
+                            }
+                        },
+                        {
+                            "type": "input",
+                            "block_id": "block_weight",
+                            "element": {
+                                "type": "plain_text_input",
+                                "action_id": "sl_input",
+                                "placeholder": {
+                                    "type": "plain_text",
+                                    "text": "750"
+                                }
+                            },
+                            "label": {
+                                "type": "plain_text",
+                                "text": "Weight"
+                            }
+                        },
+                        {
+                            "type": "input",
+                            "block_id": "block_items",
+                            "element": {
+                                "type": "plain_text_input",
+                                "action_id": "sl_input",
+                                "placeholder": {
+                                    "type": "plain_text",
+                                    "text": "450"
+                                }
+                            },
+                            "label": {
+                                "type": "plain_text",
+                                "text": "Items"
+                            }
+                        },
+                        {
+                            "type": "input",
+                            "block_id": "block_hours",
+                            "element": {
+                                "type": "plain_text_input",
+                                "action_id": "sl_input",
+                                "placeholder": {
+                                    "type": "plain_text",
+                                    "text": "7.5"
+                                }
+                            },
+                            "label": {
+                                "type": "plain_text",
+                                "text": "Hours"
+                            }
+                        }
+                    ],
+                    "type": "modal"
+                }
+            )
+            logger.info(result)
+            score_view_id = score_view_result['view']['id']
+            score_view_hash = body['view']['hash']
+        except SlackApiError as e:
+            logger.info(f'Error creating view: {e}')
 
+    @app.view("calc_score_modal")
+    def get_stats_update_modal(ack, body, view):
+        errors = {}
+        ack()
+        print(view)
+        package_count = view['state']['values']['block_package']['sl_input']['value']
+        weight_count = view['state']['values']['block_weight']['sl_input']['value']
+        item_count = view['state']['values']['block_items']['sl_input']['value']
+        hour_count = view['state']['values']['block_hours']['sl_input']['value']
+        print(package_count, weight_count, item_count, hour_count)
+
+        view_id = body['view']['id']
+        view_hash = body['view']['hash']
+        trigger_id = body['trigger_id']
+
+        try:
+            app.client.views_open(
+                # Pass the view_id
+                view_id=body["view"]["id"],
+                # String that represents view state to protect against race conditions
+                hash=body["view"]["hash"],
+                trigger_id=trigger_id,
+                # View payload with updated blocks
+                view={
+                    "type": "modal",
+                    # View identifier
+                    "callback_id": "calc_score_modal",
+                    "title": {"type": "plain_text", "text": "Updated modal"},
+                    "blocks": [
+                        {
+                            "type": "section",
+                            "text": {"type": "plain_text", "text": "You updated the modal!"}
+                        },
+                        {
+                            "type": "image",
+                            "image_url": "https://media.giphy.com/media/SVZGEcYt7brkFUyU90/giphy.gif",
+                            "alt_text": "Yay! The modal was updated"
+                        }
+                    ]
+                }
+            )
+        except SlackApiError as e:
+            print(e.response)
+
+    user = event["user"]
     try:
-        # Call the views.publish method using the WebClient passed to listeners
-        result = client.views_publish(
+        # app home view
+        result = app.client.views_publish(
             user_id=user,
             view={
                 # Home tabs must be enabled in your app configuration page under "App Home"
@@ -66,7 +204,7 @@ def app_home_opened(event, client, logger):
                                     "type": "plain_text",
                                     "text": "Productivity Score Calculator"
                                 },
-                                "action_id": "score_button"
+                                "action_id": "score_home_button"
                             },
                             {
                                 "type": "button",
@@ -74,7 +212,7 @@ def app_home_opened(event, client, logger):
                                     "type": "plain_text",
                                     "text": "Piece Pay Calculator"
                                 },
-                                "action_id": "pay_button"
+                                "action_id": "piece_pay_home_button"
                             }
                         ]
                     }
@@ -82,7 +220,10 @@ def app_home_opened(event, client, logger):
             },
         )
         logger.info(result)
-
+        home_view_id = result['view']['id']
+        home_view_hash = result['view']['hash']
+        print(home_view_id)
+        print(home_view_hash)
     except SlackApiError as e:
         logger.error(f"Error fetching conversations: {e}")
 
