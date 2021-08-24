fb7ab99588a5ab56e094a60e63d4ed6f47a55047
Christian Zagazeta <christianzagazeta@icloud.com>
2021-08-24 14:18:02 -0400

    Initial Commit

---
+++ scripts/get_error_msg_str.py
@@ -0,0 +1,7 @@
+def get_error_msg_str(command_name):
+    """
+    Error Message str with command name
+    :param command_name: str, command name
+    :return: str
+    """
+    return f'`Error: Stats not entered correctly. Enter: "/{command_name} help" for help`'
