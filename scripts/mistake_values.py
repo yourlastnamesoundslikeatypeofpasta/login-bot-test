5a05b931f19b35a1a4bfa21282f2e2393635eb3a
czagazeta <christianzagazeta@icloud.com>
2021-08-24 11:51:40 -0400

    Initial Commit

---
+++ scripts/mistake_values.py
@@ -0,0 +1,5 @@
+mistake_values = {'flex': 0, 'batt min': 0, 'pp': 0, 'packg': 0, 'we': 1, 'de': 2, 'packg2': 2, 'de2': 2,
+                  'ph2': 2, 'lu': 2, 'pp2': 2, 'osd': 4, 'hk': 4, 'da': 4, 'l-ph': 4, 'item': 4, 'tw': 4,
+                  'upc': 4, 'psl': 4, 'ms': 4, 'fsp': 4, 'mi': 4, 'ph4': 4, 'sn': 4, 'qd': 4, 'ps': 5, 'ml': 7,
+                  'wn': 7, 'sh': 8, 'fpp': 8, 'fxr': 12, 'fmd': 12, 'ffw': 12, 'fdg': 12, 'batt maj': 12,
+                  'in': 18, 'la': 18, 'wa': 18, 'cm': 22, 'utl': 25}
