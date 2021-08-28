c7443ad4d0f0683e10f6bd458bd95b4d4a5d5a21
Christian Zagazeta <christianzagazeta@icloud.com>
2021-08-28 11:55:47 -0400

    - Added methods to DiscardToken - issue_newly_generated_coins - mine

--- scripts/discard_token.py
+++ scripts/discard_token.py
@@ -9,7 +9,7 @@ from statistics import mean
 
 class DiscardToken:
     def __init__(self):
-        self.genesis_hash = self.hash_str('DISARDDDD DOLLARRRR TO THE MOONNNNNN!🚀')
+        self.genesis_hash = self.hash_str('DISKARDDDD DOLLARRRR TO THE MOONNNNNN!🚀')
         self.genesis_tokens = 99999999999999
         self.genesis_block = {
             'index': 1,
@@ -39,6 +39,7 @@ class DiscardToken:
         return print('Transaction Denied: Insufficient Balance')
 
     def add_block(self):
+        #self.issue_newly_generated_coins(winner_address, winner_reward)
         block = {
             'index': len(self.chain) + 1,
             "timestamp": time.time(),
@@ -111,6 +112,10 @@ class DiscardToken:
         transaction_amount_lst = self.get_transaction_amount_lst()
         return mean(transaction_amount_lst)
 
+    def get_total_tokens(self):
+        transaction_amount_lst = self.get_transaction_amount_lst()
+        return sum(transaction_amount_lst)
+
     def get_transaction_amount_lst(self):
         transaction_amount_lst = []
         for block in self.chain:
@@ -120,6 +125,21 @@ class DiscardToken:
                     continue
                 transaction_amount_lst.append(transaction_amount)
         return transaction_amount_lst
+
+    def issue_newly_generated_coins(self, address, reward):
+        issuance_transaction = {
+            'sender': '',
+            'recipient': address,
+            'amount': reward
+        }
+        self.current_trans.append(issuance_transaction)
+
+    def mine(self):
+        self.add_block()
+
+        if self.is_chain_valid():
+            return True
+        return False
 
     @staticmethod
     def create_wallet():
@@ -139,10 +159,10 @@ class DiscardToken:
         return block_hash
 
 
-'''
+
 # usage
 # initiate chain
-init_chain = DiscardToken()
+'''init_chain = DiscardToken()
 
 # create test wallet
 wallet = init_chain.create_wallet()
@@ -151,26 +171,30 @@ wallet = init_chain.create_wallet()
 print('original balances')
 print(init_chain.get_wallet_balance('the_kings_wallet'))
 print(init_chain.get_wallet_balance(wallet))
+print(init_chain.get_wallet_balance('miner_address'))
 print('-' * 10)
 
 # send 10 from the kings wallet to wallet address
 print('send 10 from the kings wallet to wallet address')
 init_chain.add_transaction('the_kings_wallet', wallet, 10)
-init_chain.add_block()
-if init_chain.is_chain_valid():
-    print(init_chain.get_wallet_balance('the_kings_wallet'))
-    print(init_chain.get_wallet_balance(wallet))
+init_chain.mine()
+print(init_chain.get_wallet_balance('the_kings_wallet'))
+print(init_chain.get_wallet_balance(wallet))
+print(init_chain.get_wallet_balance('miner_address'))
 print('-' * 10)
 
 # send 15000 from wallet to the kings wallet, (should throw insufficient funds error)
 print('send 15000 from wallet to the kings wallet, (should throw insufficient funds error)')
 init_chain.add_transaction(wallet, 'the_kings_wallet', 150000)
-init_chain.add_block()
+init_chain.mine()
 if init_chain.is_chain_valid():
     print(init_chain.get_wallet_balance('the_kings_wallet'))
     print(init_chain.get_wallet_balance(wallet))
+    print(init_chain.get_wallet_balance('miner_address'))'''
 
 # NOTE: if a user would like to send tokens, they need to be granted tokens from 'the_kings_wallet' first
+
+init_chain = DiscardToken()
 
 # create fake transactions, add them to a block, add the block to the chain, validate chain
 number_of_transactions = 1000
@@ -185,12 +209,13 @@ for i in range(number_of_transactions):
     random_amount = random.randint(1, 10000)
     init_chain.add_transaction(random_sender, random_receiver, random_amount)
     if i % number_of_transactions_per_block == 0:
-        init_chain.add_block()
+        init_chain.mine()
         if init_chain.is_chain_valid():
             continue
         else:
             print('Chain is invalid')
             break
+print(f'Miner Address Balance: {init_chain.get_wallet_balance("miner_address")}')
 end_amount = init_chain.get_wallet_balance(king_wallet)
 amount_sent = starting_amount - end_amount
 print(f'amount sent: {amount_sent}')
@@ -199,8 +224,8 @@ process_time = time_end - time_start
 print(f'Time to process {number_of_transactions} transactions with {number_of_transactions_per_block} transactions/block; {process_time} seconds')
 print(f'Largest transaction: {init_chain.get_largest_transaction_amount()}')
 print(f'Average transaction: {init_chain.get_average_transaction_amount()}')
+print(f'Total tokens in circulation: {init_chain.get_total_tokens()}')
 if init_chain.is_chain_valid():
     print('All blocks validated')
     pprint.pprint(init_chain.chain[-1])
     print(init_chain.get_wallet_balance(king_wallet))
-'''
