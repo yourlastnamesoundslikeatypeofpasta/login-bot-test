bf8f48002850fc360da370c1af2cad17060b8563
Christian Zagazeta <christianzagazeta@icloud.com>
2021-09-05 01:40:23 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import logging
+import os
+
+from slack_bolt import App
+from slack_sdk.errors import SlackApiError
+
+# start Slack app
+logging.basicConfig(level=logging.INFO)
+try:
+    app = App(token=os.environ['bot_token'], signing_secret=os.environ['signin_secret'])
+except SlackApiError as e:
+    logging.error(e)
