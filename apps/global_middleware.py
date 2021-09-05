f853df5b5b1d84535a0661c0fce958f612b4b2ab
Christian Zagazeta <christianzagazeta@icloud.com>
2021-09-05 18:00:34 -0400

    Added fetch_triger_id, fetch_root_id, and calculate_production_score

--- apps/global_middleware.py
+++ apps/global_middleware.py
@@ -1,4 +1,3 @@
-
 def fetch_user_id(payload, context, next, logger):
     if 'user' in payload:
         user = payload['user']
@@ -9,3 +8,29 @@ def fetch_user_id(payload, context, next, logger):
     else:
         logger.error('User not found in payload')
     next()
+
+
+def fetch_trigger_id(body, context, next):
+    trigger_id = body['trigger_id']
+    context['trigger_id'] = trigger_id
+    next()
+
+
+def fetch_root_id(body, context, next):
+    context['root_view_id'] = body['view']['root_view_id']
+    next()
+
+
+def calculate_production_score(context, next):
+    # set the points that each stat is worth
+    if 'stats' in context:
+        pkg_points = 14.7
+        item_points = 2.03
+        weight_points = 0.99
+
+        stats = context['stats']
+        score = ((pkg_points * stats['packages']) + (item_points * stats['items']) + (weight_points * stats['weight'])) / stats['hours']
+        context['score'] = score
+        next()
+    else:
+        next()
