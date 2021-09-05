11a1ac7deca7cd57a1dfbdee1d14ae568f71f574
Christian Zagazeta <christianzagazeta@icloud.com>
2021-09-05 18:11:33 -0400

    current logging level

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from slack_bolt import App
 from slack_sdk.errors import SlackApiError
 
 # start Slack app
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.DEBUG)
+
 try:
     app = App(token=os.environ['bot_token'], signing_secret=os.environ['signin_secret'])
 except SlackApiError as e:
