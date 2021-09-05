0c005829e5beff6b501ac3c5533b2339c6ffad82
Christian Zagazeta <christianzagazeta@icloud.com>
2021-09-05 14:51:18 -0400

    updated func name and fetch_user_id can now grab user ids from payloads that have the user id in 'user_id'

--- apps/global_middleware.py
+++ apps/global_middleware.py
@@ -1,5 +1,11 @@
 
-def fetch_user(payload, context, next):
-    user = payload["user"]
-    context['user'] = user
+def fetch_user_id(payload, context, next, logger):
+    if 'user' in payload:
+        user = payload['user']
+        context['user'] = user
+    elif 'user_id' in payload:
+        user = payload['user_id']
+        context['user'] = user
+    else:
+        logger.error('User not found in payload')
     next()
