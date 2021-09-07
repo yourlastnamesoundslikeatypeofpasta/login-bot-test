5e6d28bcd3f9a6976eb8e7cb9e57faf25257203a
Christian Zagazeta <christianzagazeta@icloud.com>
2021-09-06 23:08:42 -0400

    added input validation via 'global' middleware

--- apps/modal_production_calc/modal_production_calc.py
+++ apps/modal_production_calc/modal_production_calc.py
@@ -2,6 +2,7 @@ import string
 
 from app import app
 from apps.global_middleware import fetch_trigger_id
+from apps.global_middleware import validate_input
 from apps.global_middleware import calculate_production_score
 from slack_sdk.errors import SlackApiError
 
@@ -102,57 +103,8 @@ def get_input_values(context, body, next):
     next()
 
 
-def validate_input(context, next, logger):
-    # valid input = starts with digit > 0, all digits, <= 1 decimal
-    invalid_block_id_lst = []
-    digits = string.digits
-    for block_id, block_stat in context['input_block_values'].items():
-        if block_stat.count(".") >= 2:  # let 1 '.' through
-            invalid_block_id_lst.append((block_id, 'This entry cannot have more than one decimal'))
-            continue
-        elif block_stat[0] not in digits[1:]:
-            # skip block hours, user may enter '0.5' hours
-            if block_id == 'block_hours':
-                pass
-            else:
-                invalid_block_id_lst.append((block_id, 'This entry must start with a positive number'))
-                continue
-        # check all string indexes
-        for number in block_stat:
-            if number not in digits and '.' != number:  # hours may include a decimal
-                invalid_block_id_lst.append((block_id, 'This entry can only contain numbers'))
-
-    # create action response, send response action if inputs are invalid
-    if invalid_block_id_lst:
-        logger.debug('invalid blocks')
-        response_action = {
-            "response_action": "errors",
-            "errors": {}
-        }
-        for block, error in invalid_block_id_lst:
-            response_action['errors'][block] = error
-        context['response_action'] = response_action
-        next()
-        return
-    else:
-        # convert input values to floats and add values to context
-        for key, value in context['input_block_values'].items():
-            context[key] = float(value)
-        stats = {
-            'packages': context['block_packages'],
-            'weight': context['block_weight'],
-            'items': context['block_items'],
-            'hours': context['block_hours'],
-            'pkg_per_hour': context['block_packages'] / context['block_hours'],
-            'weight_per_pkg': context['block_weight'] / context['block_packages'],
-            'items_per_pkg': context['block_items'] / context['block_packages']
-        }
-        context['stats'] = stats
-        next()
-
-
-def create_blocks(context, next):
-    if 'stats' in context['stats']:
+def create_score_blocks(context, next):
+    if 'stats' in context:
         stats = context['stats']
         score = context['score']
         score_block = [
@@ -184,8 +136,8 @@ def create_blocks(context, next):
         next()
 
 
-def update_view(context, next):
-    if 'new_block' in context['new_block']:
+def update_base_view(context, next):
+    if 'new_block' in context:
 
         # create new view key
         context['view'] = context['base_view']
@@ -201,8 +153,9 @@ def update_view(context, next):
 
 
 # root view
-@app.action('production_calc_button_click',
-            middleware=[fetch_trigger_id, fetch_base_view])
+@app.action('production_calc_button_click', middleware=[fetch_trigger_id,
+                                                        fetch_base_view,
+                                                        ])
 def show_root_view(ack, context, logger):
     ack()
     trigger_id = context['trigger_id']
@@ -218,9 +171,13 @@ def show_root_view(ack, context, logger):
 
 
 # updated view
-@app.view("production_calc_submission", middleware=[fetch_base_view, get_input_values,
-                                                    validate_input, calculate_production_score,
-                                                    create_blocks, update_view])
+@app.view("production_calc_submission", middleware=[fetch_base_view,
+                                                    get_input_values,
+                                                    validate_input,
+                                                    calculate_production_score,
+                                                    create_score_blocks,
+                                                    update_base_view,
+                                                    ])
 def show_updated_view(ack, context, logger):
     ack()
     if 'response_action' in context:
